e55490aad6063168740b0dfaeecaeec89630e1c0
KSENGO06 <grigorivoksana@gmail.com>
2024-08-09 17:32:58 +0200

    зробила другу сторінку та її адаптив

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,11 @@
+
 $(function() {
+    
+    $('.select-style').styler();
+
     $('.reviews__slider-items').slick({
         dots: true,
+        infinite: false,
         prevArrow: '<button type="button" class="reviews__slider-arrow reviews__slider-arrow--prev"><svg class="reviews__slider-icon"><use xlink:href="images/sprite.svg#rew_slider"></use></svg></button>',
         nextArrow: '<button type="button" class="reviews__slider-arrow reviews__slider-arrow--next"><svg class="reviews__slider-icon"><use xlink:href="images/sprite.svg#rew_slider"></use></svg></button>',
         appendArrows: '.reviews-wrap',
@@ -14,41 +19,89 @@ $(function() {
         ]
     });
 
-    $('.reviews__slider-arrow').on('click', function() {
-        var $this = $(this);
-        $this.addClass('active');
-        setTimeout(function() {
-            $this.removeClass('active');
-        }, 3000);
+    var $range = $(".js-range-slider"),
+        $inputFrom = $(".js-input-from"),
+        $inputTo = $(".js-input-to"),
+        instance,
+        min = 50,
+        max = 1150,
+        from = 100,
+        to = 1000;
+
+    $range.ionRangeSlider({
+        skin: "round",
+        type: "double",
+        min: min,
+        max: max,
+        from: from,
+        to: to,
+        onStart: updateInputs,
+        onChange: updateInputs
     });
-});
+    instance = $range.data("ionRangeSlider");
 
-window.addEventListener('scroll', function() {
-    const header = document.querySelector('.header__inner');
-    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    function updateInputs(data) {
+        from = data.from;
+        to = data.to;
 
-    if (scrollTop > 80) {
-    header.classList.add('header__inner--fixed');
-    } else {
-    header.classList.remove('header__inner--fixed');
+        $inputFrom.prop("value", from);
+        $inputTo.prop("value", to);
     }
-});
 
-$(window). on ('load resize', function(){
-    if($(window) .width () < 768) {
-    $('.restaurants__inner:not(.slick-initialized)').slick({
-    arrows: false,
-    dots: true,
-    infinite: true,
-    speed: 100,
-    slidesToShow: 1,
+    $inputFrom.on("input", function() {
+        var val = $(this).prop("value");
+
+        if (val < min) {
+            val = min;
+        } else if (val > to) {
+            val = to;
+        }
+
+        instance.update({
+            from: val
+        });
     });
-}else {
-    $('.restaurants__inner. slick-initialized'). slick( 'unslick');
-}
-});
 
-$(document).ready(function () {
+    $inputTo.on("input", function() {
+        var val = $(this).prop("value");
+
+        if (val < from) {
+            val = from;
+        } else if (val > max) {
+            val = max;
+        }
+
+        instance.update({
+            to: val
+        });
+    });
+
+    $(window).on('load resize', function() {
+        if ($(window).width() < 768) {
+            $('.restaurants__inner:not(.slick-initialized)').slick({
+                arrows: false,
+                dots: true,
+                infinite: true,
+                speed: 100,
+                slidesToShow: 1,
+            });
+        } else {
+            $('.restaurants__inner.slick-initialized').slick('unslick');
+        }
+    });
+    $(window).on('load resize', function() {
+        if ($(window).width() < 768) {
+            $('.sale__inner:not(.slick-initialized)').slick({
+                arrows: false,
+                dots: true,
+                infinite: true,
+                speed: 100,
+                slidesToShow: 1,
+            });
+        } else {
+            $('.sale__inner.slick-initialized').slick('unslick');
+        }
+    });
     function toggleMobileMenu() {
         const burger = document.querySelector('.burger');
         const mobileMenu = document.querySelector('.mobile-menu');
@@ -58,25 +111,14 @@ $(document).ready(function () {
 
         if (burger && mobileMenu && overlay && close) {
             burger.addEventListener('click', () => {
-                
                 const mobileMenuContent = mobileMenu.querySelector('.mobile-menu__content');
                 if (mobileMenuContent.children.length === 0) {
                     const logo = document.querySelector('.logo').cloneNode(true);
                     const menu = document.querySelector('.menu').cloneNode(true);
                     const address = document.querySelector('.address') ? document.querySelector('.address').cloneNode(true) : null;
-                    
+
                     mobileMenuContent.appendChild(logo);
-
-                    const divider1 = document.createElement('div');
-                    divider1.classList.add('divider');
-                    mobileMenuContent.appendChild(divider1);
-
                     mobileMenuContent.appendChild(menu);
-
-                    const divider2 = document.createElement('div');
-                    divider2.classList.add('divider');
-                    mobileMenuContent.appendChild(divider2);
-
                     if (address) {
                         mobileMenuContent.appendChild(address);
                     }
@@ -85,7 +127,7 @@ $(document).ready(function () {
                 mobileMenu.classList.add('mobile-menu--active');
                 overlay.classList.add('overlay--active');
                 bodyLock.classList.add('lock');
-                close.style.display = 'block'; 
+                close.style.display = 'block';
                 close.classList.add('close--active');
             });
 
@@ -93,7 +135,7 @@ $(document).ready(function () {
                 mobileMenu.classList.remove('mobile-menu--active');
                 overlay.classList.remove('overlay--active');
                 bodyLock.classList.remove('lock');
-                close.style.display = 'none'; 
+                close.style.display = 'none';
                 close.classList.remove('close--active');
             });
 
@@ -101,7 +143,7 @@ $(document).ready(function () {
                 mobileMenu.classList.remove('mobile-menu--active');
                 overlay.classList.remove('overlay--active');
                 bodyLock.classList.remove('lock');
-                close.style.display = 'none'; 
+                close.style.display = 'none';
                 close.classList.remove('close--active');
             });
         }
@@ -125,6 +167,34 @@ $(document).ready(function () {
 
     $(window).on('load resize', handleResize);
     handleResize();
+
+    
+    window.addEventListener('scroll', function() {
+        const header = document.querySelector('.header__inner');
+        const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        
+        if (scrollTop > 80) {
+            header.classList.add('header__inner--fixed');
+        } else {
+            header.classList.remove('header__inner--fixed');
+        }
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    var filterToggle = document.getElementById('filterToggle');
+    var filterContent = document.getElementById('filterContent');
+    var filterClose = document.getElementById('filterClose');
+    var overlay = document.getElementById('overlay');
+
+    function toggleFilters() {
+        filterContent.classList.toggle('catalog__filters--active');
+        overlay.classList.toggle('overlay--active');
+    }
+
+    filterToggle.addEventListener('click', toggleFilters);
+    filterClose.addEventListener('click', toggleFilters);
+    overlay.addEventListener('click', toggleFilters);
 });
 
 
--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,7 @@ const autoprefixer = require("gulp-autoprefixer");
 const uglify = require("gulp-uglify");
 const imagemin = require("gulp-imagemin");
 const del = require("del");
-const browserSync = require("browser-sync").create();
+const browserSync = require('browser-sync').create();
 const svgSprite = require('gulp-svg-sprite');
 const cheerio = require('gulp-cheerio');
 const replace = require('gulp-replace');
@@ -16,41 +16,18 @@ const rename = require('gulp-rename');
 
 function browsersync() {
   browserSync.init({
-    server: {
-      baseDir: "app/",
-    },
-    notify: false,
+      server: {
+          baseDir: 'app/'
+      },
   });
 }
-gulp.task('copy-swiped-events', function() {
-  return gulp.src('node_modules/swiped-events/dist/swiped-events.min.js')
+
+function copySwipedEvents() {
+  return src('node_modules/swiped-events/dist/swiped-events.min.js')
       .pipe(rename('swiped-events.min.js'))
-      .pipe(gulp.dest('app/js/vendor'));
-});
-
-
-function svgSprites() {
-  return src('app/images/icons/*.svg') 
-  .pipe(cheerio({
-        run: ($) => {
-            $("[fill]").removeAttr("fill"); // очищаем цвет у иконок по умолчанию, чтобы можно было задать свой
-            $("[stroke]").removeAttr("stroke"); // очищаем, если есть лишние атрибуты строк
-            $("[style]").removeAttr("style"); // убираем внутренние стили для иконок
-        },
-        parserOptions: { xmlMode: true },
-      })
-  )
-	.pipe(
-	      svgSprite({
-	        mode: {
-	          stack: {
-	            sprite: '../sprite.svg', 
-	          },
-	        },
-	      })
-	    )
-	.pipe(dest('app/images')); 
+      .pipe(dest('app/js/vendor'));
 }
+
 function svgSprites() {
   return src('app/images/icons/*.svg') 
   .pipe(cheerio({
@@ -60,34 +37,18 @@ function svgSprites() {
             $("[style]").removeAttr("style"); 
         },
         parserOptions: { xmlMode: true },
-      })
-  )
-	.pipe(replace('&gt;','>')) // боремся с заменой символа 
-	.pipe(
-	      svgSprite({
-	        mode: {
-	          stack: {
-	            sprite: '../sprite.svg', 
-	          },
-	        },
-	      })
-	    )
-	.pipe(dest('app/images')); 
-}
-
-function svgSprites() {
-  return src('app/images/icons/*.svg') // вибираємо всі файли з розширенням svg у папці з іконками
-    .pipe(
-      svgSprite({
+      }))
+      .pipe(replace('&gt;', '>'))
+      .pipe(svgSprite({
         mode: {
           stack: {
-            sprite: '../sprite.svg', // вказуємо ім'я файлу спрайта та шлях
+            sprite: '../sprite.svg',
           },
         },
-      })
-    )
-    .pipe(dest('app/images')); // вказуємо, в яку папку помістити готовий файл спрайта
-}
+      }))
+      .pipe(dest('app/images'));
+  }
+  
 
 function fileIncludes() {
   return src(['src/*.html'])
@@ -127,14 +88,27 @@ function images() {
     .pipe(dest("dist/images"));
 }
 
-function build() {
-  return src(["app/**/*.html", "app/css/style.min.css", "app/js/main.min.js"], 
-    gulp.task('build', gulp.series('copy-swiped-events', 'styles', 'scripts', 'html')),
-    {
-    base: "app",
-  }).pipe(dest("dist"));
+
+function copyStyles() {
+  return src('nodemodules/nouislider/dist/nouislider.css')
+    .pipe(dest('app/css/vendor'));
 }
 
+function scripts() {
+  return src([
+    "node_modules/jquery/dist/jquery.js",
+    "node_modules/slick-carousel/slick/slick.js",
+    "node_modules/mixitup/dist/mixitup.js",
+    "node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js",
+    "node_modules/ion-rangeslider/js/ion.rangeSlider.js",
+    "node_modules/jquery-form-styler/dist/jquery.formstyler.js",
+    "app/js/main.js",
+  ])
+    .pipe(concat("main.min.js"))
+    .pipe(uglify())
+    .pipe(dest("app/js"))
+    .pipe(browserSync.stream());
+}
 function cleanDist() {
   return del("dist");
 }
@@ -145,21 +119,11 @@ function watching() {
   watch(["app/**/*.html"]).on("change", browserSync.reload);
   watch(['app/images/icons/*.svg'], svgSprites);
 }
-
-function scripts() {
-  return src([
-    "node_modules/jquery/dist/jquery.js",
-    "node_modules/slick-carousel/slick/slick.js",
-    "node_modules/mixitup/dist/mixitup.js",
-    "node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js",
-    "app/js/main.js",
-  ])
-    .pipe(concat("main.min.js"))
-    .pipe(uglify())
-    .pipe(dest("app/js"))
-    .pipe(browserSync.stream());
+function build() {
+  return src(["app/**/*.html", "app/css/style.min.css", "app/js/main.min.js"], {
+    base: "app",
+  }).pipe(dest("dist"));
 }
-
 exports.styles = styles;
 exports.scripts = scripts;
 exports.browsersync = browsersync;
@@ -169,6 +133,8 @@ exports.cleanDist = cleanDist;
 exports.build = series(cleanDist, images, build);
 exports.svgSprites = svgSprites;
 exports.fileIncludes = fileIncludes;
+exports.copyStyles = copyStyles;
 
-exports.default = parallel(svgSprites, styles, fileIncludes, scripts, browsersync, watching);
+exports.default = parallel(copySwipedEvents, styles, fileIncludes, scripts, browsersync, watching);
+
 
